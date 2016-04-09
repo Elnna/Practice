ed15449e81508a4ac52bf5847d10ad97d174f11c
Graham <huang_yajing92@126.com>
2016-04-09 08:10:53 +0800

    add works slick

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
     $(".experience").popover('show');
-     
+     $('.my-works').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
 });
