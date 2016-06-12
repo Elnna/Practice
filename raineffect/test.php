51c37c9978af466c923ca351d59f9aa09c2e7dcb
Graham <huang_yajing92@126.com>
2016-06-12 14:57:38 +0800

    test

--- raineffect/test.php
+++ raineffect/test.php
@@ -6,7 +6,7 @@ require_once('./api/weather.code.php'); //引入天气标识
 
 //接口基本信息配置
 $appkey = '895e98a1c9681cae048688ef98feffec'; //全国天气查询appkey
-//$weather = new weather($appkey);
+$weather = new weather($appkey);
 //根据IP查询天气
 //$ipWeatherResult = $weather->getWeatherByIP(getIp());
 echo '当前IP' . getIp();
