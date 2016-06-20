678ee8ee3284495889afd81b93f77a9f2befeebb
Graham <huang_yajing92@126.com>
2016-06-20 22:38:50 +0800

    add zodiac search page

--- bd-apis/js/main.js
+++ bd-apis/js/main.js
@@ -257,6 +257,59 @@ $(document).ready(function() {
         changeWeather(city);
 
     });
+    
+    /*
+    <form class="form-horizontal">
+        <div class="form-group">
+            <label for="focusedinput" class="col-sm-2 control-label">Focused Input</label>
+            <div class="col-sm-8">
+                <input type="text" class="form-control1" id="focusedinput" placeholder="Default Input">
+            </div>
+            <div class="col-sm-2">
+                <p class="help-block">Your help text!</p>
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="disabledinput" class="col-sm-2 control-label">Disabled Input</label>
+            <div class="col-sm-8">
+                <input disabled="" type="text" class="form-control1" id="disabledinput" placeholder="Disabled Input">
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="inputPassword" class="col-sm-2 control-label">Password</label>
+            <div class="col-sm-8">
+                <input type="password" class="form-control1" id="inputPassword" placeholder="Password">
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="checkbox" class="col-sm-2 control-label">Checkbox</label>
+            <div class="col-sm-8">
+                <div class="checkbox-inline1"><label><input type="checkbox"> Unchecked</label></div>
+                <div class="checkbox-inline1"><label><input type="checkbox" checked=""> Checked</label></div>
+                <div class="checkbox-inline1"><label><input type="checkbox" disabled=""> Disabled Unchecked</label></div>
+                <div class="checkbox-inline1"><label><input type="checkbox" disabled="" checked=""> Disabled Checked</label></div>
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="checkbox" class="col-sm-2 control-label">Checkbox Inline</label>
+            <div class="col-sm-8">
+                <div class="checkbox-inline"><label><input type="checkbox"> Unchecked</label></div>
+                <div class="checkbox-inline"><label><input type="checkbox" checked=""> Checked</label></div>
+                <div class="checkbox-inline"><label><input type="checkbox" disabled=""> Disabled Unchecked</label></div>
+                <div class="checkbox-inline"><label><input type="checkbox" disabled="" checked=""> Disabled Checked</label></div>
+            </div>
+        </div>
+    </form>
+    */
+    /*$('.zodiac-img').hover(
+        function(){
+            var pageZodiac = $('.page-zodiac');
+            var newPgae = "<div class='page-search-zodiac'></div>"
+        },
+        function(){
+            
+        }
+    );*/
    
     
 });
