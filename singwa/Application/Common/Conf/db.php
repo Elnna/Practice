efec482b2369738ccafb97a8bdec48236944212e
graham <grahamhuang126.com>
2016-08-11 23:51:21 -0400

    change db config

--- singwa/Application/Common/Conf/db.php
+++ singwa/Application/Common/Conf/db.php
@@ -6,7 +6,7 @@ return array(
     'DB_USER' => 'c1voguem',
     'DB_PWD' => 'nxZGWSyibY_5',
     'DB_POST' => 3306,
-    'DB_NAME' => 'imooc_singcms',
+    'DB_NAME' => 'c1imooc_singcms',
     'DB_PREFIX' => 'cms_',
     'DB_CHARSET' => 'utf8',
 );
