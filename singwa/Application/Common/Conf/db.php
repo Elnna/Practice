35e37911a42cecdaf50b3914b9a90ca0e892efa4
graham <grahamhuang126.com>
2016-09-05 00:21:15 -0400

    change db config

--- singwa/Application/Common/Conf/db.php
+++ singwa/Application/Common/Conf/db.php
@@ -3,12 +3,10 @@ return array(
 
     'DB_TYPE' => 'mysql',
     'DB_HOST' => 'localhost',
-    'DB_USER' => 'root',
-    'DB_PWD' => '',
-//    'DB_PWD' => 'nxZGWSyibY_5',
+    'DB_USER' => 'c1voguem',
+    'DB_PWD' => 'nxZGWSyibY_5',
     'DB_POST' => 3306,
-//    'DB_NAME' => 'c1imooc_singcms',
-    'DB_NAME' => 'imooc_singcms',
+    'DB_NAME' => 'c1imooc_singcms',
     'DB_PREFIX' => 'cms_',
     'DB_CHARSET' => 'utf8',
 );
