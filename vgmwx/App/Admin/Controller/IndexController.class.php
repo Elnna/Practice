cc08ce6c351032272c20750e3da926a6ac8ea99f
Graham <huang_yajing92@126.com>
2016-09-05 17:29:17 +0800

    connect wx

--- vgmwx/App/Admin/Controller/IndexController.class.php
+++ vgmwx/App/Admin/Controller/IndexController.class.php
@@ -1,12 +1,29 @@
 <?php
 namespace Admin\Controller;
 use Think\Controller;
-use \wechatCallbackapi;
+
 
 class IndexController extends Controller {
     public function index(){
-        $wechatObj = new wechatCallbackapi();
-        $wechatObj->valid();
+       if (!defined("WX_ACCESS_TOKEN")) {
+            throw new Exception('TOKEN is not defined!');
+        }
+        
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
+        $echoStr = $_GET["echostr"];		
+		$token = WX_ACCESS_TOKEN;
+		$tmpArr = array($token, $timestamp, $nonce);
+        // use SORT_STRING rule
+		sort($tmpArr, SORT_STRING);
+		$tmpStr = implode( $tmpArr );
+		$tmpStr = sha1( $tmpStr );
+		
+		if( $tmpStr == $signature ){
+			echo $echoStr;
+            exit;
+		}
     }
     
     
