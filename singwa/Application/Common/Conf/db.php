f44cfc76a95738edcf5cc2754676c4e78b49f94b
Graham <huang_yajing92@126.com>
2016-09-05 11:47:30 +0800

    add db config

---
+++ singwa/Application/Common/Conf/db.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => 'localhost',
+    'DB_USER' => 'c1voguem',
+    'DB_PWD' => 'nxZGWSyibY_5',
+    'DB_POST' => 3306,
+    'DB_NAME' => 'imooc_singcms',
+    'DB_PREFIX' => 'cms_',
+    'DB_CHARSET' => 'utf8',
+);
