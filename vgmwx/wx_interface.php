b0dad39bce39830c89e97a1e05a8d16e026ede6a
Graham <huang_yajing92@126.com>
2016-09-06 17:32:59 +0800

    add text focus in wx

--- vgmwx/wx_interface.php
+++ vgmwx/wx_interface.php
@@ -1,6 +1,35 @@
 <?php
+include_once('wx_tpl.php');
+//获取微信发送数据
+$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+if(!empty($postStr)){
+    //解析xml数据
+    $postObj = simplexml_load_string($postStr,'SimpleXMLElement',LIBXML_NOCDATA);
+    //发送消息方ID
+    $fromUserName = $postObj->FromUserName;
+    //接收方ID
+    $toUserName = $postObj->ToUserName;
+    //消息类型
+    $msgType = $postObj->MsgType;
+    //事件消息
+    if(!$msgType == 'event'){
+        $event = $postObj->Event;
+        if($event == "subscribe"){
+            $type = "text";
+            $content = "感谢关注WeiZhi伟志服饰[愉快]！";
+            $time = time();
+            $info = sprintf($textTpl,$fromUserName,$toUserName,$time,$type,$content);
+            echo $info;
+            exit;
+            
+        }
+    }
+}else{
+    echo "Post数据为空";
+    exit;
+}
 
-function wx_interface(){
+/*function wx_interface(){
     $echoStr = $_GET["echostr"];
     $signature = $_GET["signature"];
     $timestamp = $_GET["timestamp"];
@@ -18,4 +47,4 @@ function wx_interface(){
         exit;
     }
 }
-wx_interface();
+wx_interface();*/
---
+++ vgmwx/wx_tpl.php
@@ -0,0 +1,154 @@
+<?php
+//文本消息模板
+$textTpl =  "<xml>
+             <ToUserName><![CDATA[%s]]></ToUserName>
+             <FromUserName><![CDATA[%s]]></FromUserName>
+             <CreateTime>%s</CreateTime>
+             <MsgType><![CDATA[%s]]></MsgType>
+             <Content><![CDATA[%s]]></Content>
+             </xml>";
+
+//图片消息模板
+$imageTpl =  "<xml>
+             <ToUserName><![CDATA[%s]]></ToUserName>
+             <FromUserName><![CDATA[%s]]></FromUserName>
+             <CreateTime>%s</CreateTime>
+             <MsgType><![CDATA[%s]]></MsgType>
+             <PicUrl><![CDATA[%s]]></PicUrl>
+             <MediaId><![CDATA[%s]]></MediaId>
+             </xml>";
+
+//语音消息模板
+$voiceTpl = "<xml>
+            <ToUserName><![CDATA[%s]]></ToUserName>
+            <FromUserName><![CDATA[%s]]></FromUserName>
+            <CreateTime>%s</CreateTime>
+            <MsgType><![CDATA[%s]]></MsgType>
+            <MediaId><![CDATA[%s]]></MediaId>
+            <Format><![CDATA[%s]]></Format>
+            <MsgId>%s</MsgId>
+            </xml>";
+
+
+//开启语音识别模板
+$voiceOpenTpl = "<xml>
+                <ToUserName><![CDATA[%s]]></ToUserName>
+                <FromUserName><![CDATA[%s]]></FromUserName>
+                <CreateTime>%s</CreateTime>
+                <MsgType><![CDATA[%s]]></MsgType>
+                <MediaId><![CDATA[%s]]></MediaId>
+                <Format><![CDATA[%s]]></Format>
+                <Recognition><![CDATA[腾讯微信团队]]></Recognition>
+                <MsgId>%s</MsgId>
+                </xml>";
+
+//视频消息
+$videoTpl = "<xml>
+            <ToUserName><![CDATA[%s]]></ToUserName>
+            <FromUserName><![CDATA[%s]]></FromUserName>
+            <CreateTime>%s</CreateTime>
+            <MsgType><![CDATA[%s]]></MsgType>
+            <MediaId><![CDATA[%s]]></MediaId>
+            <ThumbMediaId><![CDATA[%s]]></ThumbMediaId>
+            <MsgId>%s</MsgId>
+            </xml>";
+
+//小视频消息
+
+$smallVideoTpl ="<xml>
+                <ToUserName><![CDATA[%s]]></ToUserName>
+                <FromUserName><![CDATA[%s]]></FromUserName>
+                <CreateTime>%s</CreateTime>
+                <MsgType><![CDATA[%s]]></MsgType>
+                <MediaId><![CDATA[%s]]></MediaId>
+                <ThumbMediaId><![CDATA[%s]]></ThumbMediaId>
+                <MsgId>%s</MsgId>
+                </xml>";
+
+
+//地理位置
+$localtionTpl ="<xml>
+            <ToUserName><![CDATA[%s]]></ToUserName>
+            <FromUserName><![CDATA[%s]]></FromUserName>
+            <CreateTime>%s</CreateTime>
+            <MsgType><![CDATA[%s]]></MsgType>
+            <Location_X>%s</Location_X>
+            <Location_Y>%s</Location_Y>
+            <Scale>%s</Scale>
+            <Label><![CDATA[%s]]></Label>
+            <MsgId>%s</MsgId>
+            </xml>";
+    
+    
+    
+    
+// 链接消息
+
+$linkTpl =  "<xml>
+            <ToUserName><![CDATA[%s]]></ToUserName>
+            <FromUserName><![CDATA[%s]]></FromUserName>
+            <CreateTime>%s</CreateTime>
+            <MsgType><![CDATA[%s]]></MsgType>
+            <Title><![CDATA[%s]]></Title>
+            <Description><![CDATA[%s]]></Description>
+            <Url><![CDATA[%s]]></Url>
+            <MsgId>%s</MsgId>
+            </xml>";
+    
+
+/*------------------事件-----------------*/
+
+//关注、取消事件
+$eventTpl = "<xml>
+            <ToUserName><![CDATA[%s]]></ToUserName>
+            <FromUserName><![CDATA[%s]]></FromUserName>
+            <CreateTime>%s</CreateTime>
+            <MsgType><![CDATA[%s]]></MsgType>
+            <Event><![CDATA[%s]]></Event>
+            </xml>";
+
+//扫描二维码 
+//未关注
+$scanSqrcodeUTpl =  "<xml>
+                    <ToUserName><![CDATA[%s]]></ToUserName>
+                    <FromUserName><![CDATA[%s]]></FromUserName>
+                    <CreateTime>%s</CreateTime>
+                    <MsgType><![CDATA[event]]></MsgType>
+                    <Event><![CDATA[subscribe]]></Event>
+                    <EventKey><![CDATA[%s]]></EventKey>
+                    <Ticket><![CDATA[%s]]></Ticket>
+                    </xml>";
+
+
+//已关注
+$scanSqrcodeTpl ＝  "<xml>
+                    <ToUserName><![CDATA[%s]]></ToUserName>
+                    <FromUserName><![CDATA[%s]]></FromUserName>
+                    <CreateTime>%s</CreateTime>
+                    <MsgType><![CDATA[event]]></MsgType>
+                    <Event><![CDATA[SCAN]]></Event>
+                    <EventKey><![CDATA[%s]]></EventKey>
+                    <Ticket><![CDATA[%s]]></Ticket>
+                    </xml>";
+
+
+//自定义菜单
+
+$menuDiyTpl =   "<xml>
+                <ToUserName><![CDATA[%s]]></ToUserName>
+                <FromUserName><![CDATA[%s]]></FromUserName>
+                <CreateTime>%s</CreateTime>
+                <MsgType><![CDATA[event]]></MsgType>
+                <Event><![CDATA[CLICK]]></Event>
+                <EventKey><![CDATA[%s]]></EventKey>
+                </xml>";
+
+//点菜单跳转
+$menuClickTpl = "<xml>
+                <ToUserName><![CDATA[%s]]></ToUserName>
+                <FromUserName><![CDATA[%s]]></FromUserName>
+                <CreateTime>%s</CreateTime>
+                <MsgType><![CDATA[event]]></MsgType>
+                <Event><![CDATA[VIEW]]></Event>
+                <EventKey><![CDATA[%s]]></EventKey>
+                </xml>";
