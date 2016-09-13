d8bcd7b1c612e60480b57c38f6b4c471929d6f3f
Graham <huang_yajing92@126.com>
2016-09-13 09:32:06 +0800

    comment useless code

--- vgmwx/index.php
+++ vgmwx/index.php
@@ -1,9 +1,11 @@
 <?php
+/*
 require_once('./wx_interface.php');
 
 $wechatObj = new wechatCallbackapi();
 $token = 'weizhi';
 $wechatObj->valid($token);
+*/
 
 echo 'hello wx interface';
 phpinfo();
